5bb94c07377951fc3d97ebb16e3b92a037aff370
233703rahul <233703@ust-global.com>
2022-10-14 14:33:44 +0530

    12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todos';
+  title = 'first_project';
+  a:any[] =[]
+
+  h:any
+
+  j:any
+
+  b:any[] =[]
+
+          fun(t:any,d:any)
+
+         {                    
+
+              this.h="Event Name"
+
+              this.j="Date"
+
+              this.a.push({t})
+
+              this.b.push({d})
+
+         }
+
+         delete1(i:any){
+
+          this.a.splice(i,1)
+
+         }
+  
 }
